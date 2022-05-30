05cf53c8d8570949e84921375fcca88ad55769be
psmanideep <psmanideep@gmail.com>
2022-05-30 14:17:18 +0530

    test files and coverage updated

--- src/app/services/weather.service.spec.ts
+++ src/app/services/weather.service.spec.ts
@@ -1,16 +1,34 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { WeatherService } from './weather.service';
 
 describe('WeatherService', () => {
   let service: WeatherService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(WeatherService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call getWeatherData', () => {
+    service.getWeatherData('kakinada');
+    let payLoad = 'Kakinada';
+    spyOn(service, 'getWeatherData').and.callThrough();
+    service.getWeatherData(payLoad);
+    expect(service.getWeatherData).toHaveBeenCalled();
+  });
+
+  it('should call getWeatherData:: failure', () => {
+    service.getWeatherData('kakinada');
+    let payLoad = '@312##!';
+    spyOn(service, 'getWeatherData').and.callThrough();
+    service.getWeatherData(payLoad);
+    expect(service.getWeatherData).toThrowError();
   });
 });
