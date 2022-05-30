1b67a8ea2505255d2d707d224443e68ddccce902
psmanideep <psmanideep@gmail.com>
2022-05-30 11:18:55 +0530

    facade service test cases added

--- src/app/services/weather-facade.service.spec.ts
+++ src/app/services/weather-facade.service.spec.ts
@@ -1,29 +1,69 @@
 import { async, TestBed, waitForAsync } from '@angular/core/testing';
-import { Store } from '@ngrx/store';
 import { State } from '../store/models/state.model';
 import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { WeatherFacadeService } from './weather-facade.service';
+import * as searchAction from '../store/actions/search.action';
+import * as weatherSelector from '../store/selectors/weather.selector';
+import { MemoizedSelector } from '@ngrx/store';
+import { Observable, of } from 'rxjs';
 
 describe('WeatherFacadeService', () => {
   let service: WeatherFacadeService;
-  let store: Store<State>;
+  let store: MockStore<State>
   const initialState = {};
+  let mockLocationSelector: MemoizedSelector<State, string>;
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        providers: [provideMockStore({ initialState })],
-      }).compileComponents();
-    })
-  );
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  beforeEach(waitForAsync(() => {   
+    
+    TestBed.configureTestingModule({
+      providers: [
+        provideMockStore({ initialState }),
+      ],
+    }).compileComponents();   
     service = TestBed.inject(WeatherFacadeService);
-    store = TestBed.inject(Store);
-  });
+    store = TestBed.inject(MockStore);
+    spyOn(store, 'dispatch').and.callThrough();
+    spyOn(store, 'select').and.callThrough();
+  }));
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should setLocation', () => {
+    let location: string = "kakinada";
+    service.setLocation(location);
+    const action = searchAction.setLocation({ location });
+    expect(store.dispatch).toHaveBeenCalledWith(action);
+  });
+
+  it('should getWeatherData', () => {
+    let location: string = "kakinada";
+    service.getWeatherData(location);
+    const action = searchAction.getWeatherData({ payload:location });
+    expect(store.dispatch).toHaveBeenCalledWith(action);
+  });
+  it('should getLocation$', () => {
+    let location: string = "kakinada";
+    let mock_data = new Observable<string>();
+    service.getLocation$;
+    const selctor = weatherSelector.getLocation;
+    expect(selctor).not.toBeNull();
+    expect(store.select).toBeTruthy();
+  });
+
+  it('should getWeatherData$', () => {
+    service.getWeatherData$;
+    const selctor = weatherSelector.getWeatherData;
+    expect(selctor).not.toBeNull();
+    expect(store.select).toBeTruthy();
+  });
+
+  it('should getErrorMessage$', () => {
+    service.getErrorMessage$;
+    const selctor = weatherSelector.getErrorMessage;
+    expect(selctor).not.toBeNull();
+    expect(store.select).toBeTruthy();
   });
 });
